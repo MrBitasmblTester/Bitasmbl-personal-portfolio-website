825d9b90e457c6f2e55ed6a2e50e40d5d09ebd99
MrBitasmblTester <21100302@ibsu.edu.ge>
2025-11-06 16:08:50 +0400

    Add index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// Homepage
+import About from "../components/About";
+import Projects from "../components/Projects";
+import Contact from "../components/Contact";
+export default function Home() {
+  return (
+    <main>
+      <About />
+      <Projects />
+      <Contact />
+    </main>
+  );
+}
