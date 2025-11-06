8fe537bb58ee580448330ba0508da440ee87937d
MrBitasmblTester <21100302@ibsu.edu.ge>
2025-11-06 16:08:52 +0400

    Add Contact.js

---
+++ components/Contact.js
@@ -0,0 +1,9 @@
+// Contact section
+export default function Contact() {
+  return (
+    <section id="contact">
+      <h2>Contact Me</h2>
+      <a href="mailto:you@example.com">Send Email</a>
+    </section>
+  );
+} 
